d36dd6e8ffe09a922d52b6265f4faac0646cbc86
Batuhan TUNÇAY <batuhantunayy@hotmail.com>
2023-08-28 17:21:40 +0300

    form control

--- src/components/AddToDoForm.js
+++ src/components/AddToDoForm.js
@@ -1,5 +1,5 @@
 import { useState,useContext } from "react";
-import { InputGroup, Form, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import CreateApi from "../api/CreateApi";
 import { ToDoContext } from "../contexts/ToDoContext";
 
@@ -10,7 +10,8 @@ const AddToDoForm = ({setShowAlert,setShowAlertFailed}) => {
     const [content, setContent] = useState("");
     const { CreateToDo } = useContext(ToDoContext);
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         try {
             const addToDo = await CreateApi(title, content);
             CreateToDo(addToDo);
@@ -22,17 +23,32 @@ const AddToDoForm = ({setShowAlert,setShowAlertFailed}) => {
     }
     
     return (
-        <div>
-            <InputGroup>
-                <Form.Control className="mb-2" type="Text" placeholder="Başlık *" value={title} onChange={e => setTitle(e.target.value)} required />
-            </InputGroup>
-            <InputGroup>
-                <Form.Control className="mb-2" as="textarea" rows={10} placeholder="İçerik *" value={content} onChange={e => setContent(e.target.value)} required />
-            </InputGroup>
-            <Button className="ml-auto w-100" veriant="success" onClick={handleSubmit}>
+           <Form onSubmit={handleSubmit}>
+            <Form.Group>
+                <Form.Control
+                    className="mb-2"
+                    type="Text"
+                    placeholder="Başlık *"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    required
+                />
+            </Form.Group>
+            <Form.Group>
+                <Form.Control
+                    className="mb-2"
+                    as="textarea"
+                    rows={10}
+                    placeholder="İçerik *"
+                    value={content}
+                    onChange={e => setContent(e.target.value)}
+                    required
+                />
+            </Form.Group>
+            <Button className="ml-auto w-100" veriant="success" type="submit">
                 Ekle
             </Button>
-        </div>
+        </Form>
     )
 }
 
